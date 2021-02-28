bb86938ea746d8bdbeea28ab47fe917b582609f4
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-02-28 22:08:32 +0300

    4 пункт

--- Homework4.cpp
+++ Homework4.cpp
@@ -3,18 +3,36 @@
 #include <vector>
 #include <algorithm>
 
+void print(std::vector<int> v)
+{
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << std::endl;
+    }
+}
+
+void repeat_remover(std::vector<int> &v, int n) // удаляем повторяющиеся элементы
+{
+    int count = std::count_if(std::begin(v), std::end(v), [n](int elem) {return elem == n; });
+    if (count != 1)
+    {
+        auto it = std::find_if(std::begin(v), std::end(v), [n](int elem) {return elem == n; });
+        v.erase(std::remove_if(it + 1, std::end(v), [n](int elem) {return elem == n; }), std::end(v));
+    }
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    std::vector <int> p1(11);
-    for (size_t i = 1; i <= 10; i++)
+    std::vector <int> p1;
+    for (size_t i = 0; i < 10; ++i)
     {
-        p1[i] = i;
+        p1.push_back(i + 1);
     }
     int N = 0;
     std::cout << "Сколько чисел хотите ввести?" << std::endl;
     std::cin >> N;
-    for (size_t i = 0; i < N; i++)
+    for (size_t i = 0; i < N; ++i)
     {
         int a = 0;
         std::cout << "Введите " << i + 1 << "-ое число: " << std::endl;
@@ -22,13 +40,22 @@ int main()
         p1.push_back(a);
     }
 
-    std::random_shuffle(std::begin(p1), std::end(p1)); // перемешиваем в случайном порядке
+    std::cout << std::endl;
 
-    //std::cout << "Последовательность П1: " << std::endl;
-    //for (size_t i = 1; i < p1.size(); i++)
-    //{
-    //    std::cout << p1[i] << std::endl;
-    //}
+    std::cout << "Последовательность П1: " << std::endl;
+    print(p1);
+    std::cout << std::endl;
+
+    //std::random_shuffle(std::begin(p1), std::end(p1)); // перемешиваем в случайном порядке
+    //print(p1);
+    //std::cout << std::endl;
+
+    /*for (size_t i = 0; i < p1.size(); i++)
+    {
+        repeat_remover(p1, p1[i]);
+    }*/
+    std::cout << "Последовательность П1: " << std::endl;
+    print(p1);
     return EXIT_SUCCESS;
 }
 
