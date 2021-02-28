daa919fef28f4c313f137017c5ce945b60fd7ce5
MaslennikovDmitriy <Maslennikov.DV@phystech.edu>
2021-02-28 20:57:17 +0300

    1 пункт

--- Homework4.cpp
+++ Homework4.cpp
@@ -1,7 +1,15 @@
 ﻿#include <iostream>
+#include <clocale>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+    std::vector <int> p1(11);
+    for (size_t i = 1; i <= 10; i++)
+    {
+        p1[i] = i;
+    }
+    return EXIT_SUCCESS;
 }
 
